38f17c90618295feb8f02c0ebcd8f8866684e41e
louis <liuzizhen1234@gmail.com>
2020-09-13 21:53:44 -0400

    added server response and response code

---
+++ src/main/java/com/cyc/controller/backend/PersonalManageController.java
@@ -0,0 +1,10 @@
+package com.cyc.controller.backend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("manage/personal/")
+public class PersonalManageController {
+
+}
